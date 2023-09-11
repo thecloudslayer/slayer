445d3060cfed9535cc3de840adc21571bc160cf4
Taylor Graham <taylor.c.graham@gmail.com>
2023-09-10 22:00:55 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const { gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
+const Aerospike = require('aerospike');
+
+
+const typeDefs = gql`
+    type User {
+        id: ID!
+        name: String!
+        age: Int!
+        car: String!
+    }
+    input UserInput {
+        id: ID!
+        name: String!
+        age: Int!
+        car: String!
+    }
+
+    type Mutation {
+        addUser(input: UserInput!): User
+    }
+
+    type Query {
+        user(id: ID!): User
+    }
+
+    type Query {
+        allUsers: [User!]!
+    }
+`;
+
+
+const config = {hosts: '127.0.0.1:3000', useAlternateAccessAddress: true,};
+
+
+
+//create a aerospike reslover query for getting a userid from Aerospike database
+
+
+
+const resolvers = {
+    Query: {
+        user: async (_, { id }) => {
+            try {
+                let client = await Aerospike.connect(config);
+                const key = new Aerospike.Key('test', 'users', id);
+                const record = await client.get(key);
+                return { id, ...record.bins };
+
+
+               
+
+
+            } catch (error) {
+                console.error(error);
+                throw new Error('Failed to fetch user');
+            }
+        },
+        allUsers: async () => {
+            try {
+                let client = await Aerospike.connect(config);
+                const users = [];
+                const scan = client.scan('test', 'users'); // 'test' is the namespace, 'users' is the set
+                const stream = scan.foreach();
+
+                return new Promise((resolve, reject) => {
+                    stream.on('data', record => {
+                        users.push({ id: record.key.digest.toString('base64'), ...record.bins });
+                    });
+                    stream.on('error', error => {
+                        reject(error);
+                    });
+                    stream.on('end', () => {
+                        resolve(users);
+                    });
+                });
+            } catch (error) {
+                console.error(error);
+                throw new Error('Failed to fetch all users');
+            }
+        }
+    },
+    Mutation: {
+        addUser: async (_, { input }) => {
+            try {
+                let client = await Aerospike.connect(config);
+                const key = new Aerospike.Key('test', 'users', input.id);
+                await client.put(key, input);
+                return input;
+            } catch (error) {
+                console.error(error);
+                throw new Error('Failed to add user');
+            }
+        }
+    }
+};
+
+
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
+
